af6a097c2f3c2e34711599e7ad7711a1a76c9ee1
Subhani-syed <subhani6352@gmail.com>
2022-01-29 23:39:02 +0530

    Added Other Endpoints

--- app.js
+++ app.js
@@ -14,12 +14,13 @@ app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
 app.get("/",(req,res)=>{
-    res.render("home");
+    res.send("Welcome");
 });
 
 // Testing Endpoint
 const url = "https://global-warming.org/api/temperature-api";
-
+const methane_url = "https://global-warming.org/api/methane-api";
+const poalr_url = "https://global-warming.org/api/arctic-api";
 
 app.get("/tmp",(req,res)=>{
     let labless = [];
@@ -28,16 +29,12 @@ app.get("/tmp",(req,res)=>{
         .then(response=>{
             let out = response.data.result;
             out.forEach((ot)=>{
-                // if(ot.time>='1880'){
-                    // labless.push(Number(ot.time));
-                    // valuess.push(Number(ot.station));    
-                // }
                 labless.push(Number(ot.time));
                 valuess.push(Number(ot.station));
             })
-            console.log(labless);
-            console.log("This is valuess ");
-            console.log(valuess);
+            // console.log(labless);
+            // console.log("This is valuess ");
+            // console.log(valuess);
             res.render("home",{
                 val:valuess,
                 lab:labless
@@ -48,7 +45,47 @@ app.get("/tmp",(req,res)=>{
         });
 });
 
+// Route for Methane Emission
+app.get("/methane",(req,res)=>{
+    let methane_labels = [];
+    let methane_values = [];
+    axios.get(methane_url)
+        .then(response=>{
+            let output = response.data.methane;
+            output.forEach((opt)=>{
+                methane_labels.push(Number(opt.date));
+                methane_values.push(Number(opt.average));
+            })
+            res.render("methane",{
+                val:methane_values,
+                lab:methane_labels
+            })
+        })
+        .catch(error=>{
+            console.log(error);
+        })
+});
 
+// Route for Polar Caps
+app.get("/polar",(req,res)=>{
+    let pol_lab = [];
+    let pol_val = [];
+    axios.get(poalr_url)
+        .then(response=>{
+            let polar_result =response.data.result;
+            polar_result.forEach((pol)=>{
+                pol_lab.push(Number(pol.year));
+                pol_val.push(Number(pol.area));
+            })
+            res.render("polar",{
+                val:pol_val,
+                lab:pol_lab
+            })
+        })
+        .catch(error=>{
+            console.log(error);
+        })
+})
 app.listen(3000,()=>{
     console.log("App is running!!");
 })
