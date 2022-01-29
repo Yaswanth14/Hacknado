8f976f77364b36dcb9b51e398f805681cbe51df0
Subhani-syed <subhani6352@gmail.com>
2022-01-29 19:47:35 +0530

    Added Chartjs

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require('express');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const path = require("path");
+const https = require('https');
 const app = express();
+const axios = require('axios');
+const { response } = require('express');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 // To Use CSS
@@ -12,7 +15,37 @@ app.set("view engine", "ejs");
 
 app.get("/",(req,res)=>{
     res.render("home");
-})
+});
+
+const url = "https://global-warming.org/api/temperature-api";
+
+
+app.get("/tmp",(req,res)=>{
+    let labless = [];
+    let valuess = [];
+    axios.get(url)
+        .then(response=>{
+            let out = response.data.result;
+            out.forEach((ot)=>{
+                // if(ot.time>='1880'){
+                    // labless.push(Number(ot.time));
+                    // valuess.push(Number(ot.station));    
+                // }
+                labless.push(Number(ot.time));
+                valuess.push(Number(ot.station));
+            })
+            console.log(labless);
+            console.log("This is valuess ");
+            console.log(valuess);
+            res.render("home",{
+                val:valuess,
+                lab:labless
+            })
+        })
+        .catch(error=>{
+            console.log(error);
+        });
+});
 
 
 app.listen(3000,()=>{
